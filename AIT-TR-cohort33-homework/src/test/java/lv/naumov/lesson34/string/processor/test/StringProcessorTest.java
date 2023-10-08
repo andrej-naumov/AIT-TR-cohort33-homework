66f97f3edb7ff68896d5cca1b1a8535fba18bdda
naumo <andrejs@list.ru>
2023-10-08 19:10:06 +0200

    add lesson 34

---
+++ AIT-TR-cohort33-homework/src/test/java/lv/naumov/lesson34/string/processor/test/StringProcessorTest.java
@@ -0,0 +1,43 @@
+package lv.naumov.lesson34.string.processor.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import lv.naumov.lesson34.string.processor.StringProcessorPlus;
+
+public class StringProcessorTest {
+
+    private StringProcessorPlus stringProcessor;
+
+    @BeforeEach
+    public void setUp() {
+        stringProcessor = new StringProcessorPlus();
+    }
+
+    @Test
+    public void testConcatenate() {
+        String result = stringProcessor.concatenate("Hello, ", "world!");
+        assertEquals("Hello, world!", result);
+    }
+
+    @Test
+    public void testGetLength() {
+        int length = stringProcessor.getLength("Hello");
+        assertEquals(5, length);
+    }
+
+    @Test
+    public void testReverse() {
+        String reversed = stringProcessor.reverse("Java");
+        assertEquals("avaJ", reversed);
+    }
+
+    @Test
+    public void testIsPalindrome() {
+        assertTrue(stringProcessor.isPalindrome("А роза упала на лапу Азора"));
+        assertFalse(stringProcessor.isPalindrome("Привет"));
+    }
+}
+
