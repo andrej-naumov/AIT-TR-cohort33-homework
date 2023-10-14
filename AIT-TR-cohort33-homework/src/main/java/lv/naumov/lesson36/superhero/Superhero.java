21f4d50e767b698ba326f2762aa66e694c9737f8
naumo <naumo@DESKTOP-L200M8U>
2023-10-14 22:23:58 +0200

    added lesson 36 + tests

--- AIT-TR-cohort33-homework/src/main/java/lv/naumov/lesson36/superhero/Superhero.java
+++ AIT-TR-cohort33-homework/src/main/java/lv/naumov/lesson36/superhero/Superhero.java
@@ -4,9 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
-
-public class SuperHero {
-	@Getter @Setter	
+@Slf4j
+public class Superhero {
+	@Getter
+	@Setter
 	private String name;
 
 	@Getter
@@ -16,12 +17,32 @@ public class SuperHero {
 	@Setter
 	private int age;
 
+	public Superhero(String name, int power, int age) {
+		this.name = name;
+		// Убедимся, что уровень силы находится в диапазоне от 1 до 10
+		if (power >= 1 && power <= 10) {
+			this.power = power;
+		} else {
+			// Если значение не входит в диапазон, установим по умолчанию 5
+			log.error("уровень силы не находится в диапазоне от 1 до 10");
+			log.info("значение силы установлено по умолчанию 5");
+			this.power = 5;
+		}
+		this.age = age;
+	}
+
 	public void setPower(int power) {
 		// Убедимся, что уровень силы находится в диапазоне от 1 до 10.
 		if (power >= 1 && power <= 10) {
 			this.power = power;
 		} else {
-			
+			log.error("уровень силы не находится в диапазоне от 1 до 10");
 		}
+	}
+
+	// Переопределение метода toString
+	@Override
+	public String toString() {
+		return "Имя: " + name + ", Сила: " + power + ", Возраст: " + age;
 	}
 }
---
+++ AIT-TR-cohort33-homework/src/test/java/lv/naumov/lesson36/superhero/test/SuperheroManagerTest.java
@@ -0,0 +1,123 @@
+package lv.naumov.lesson36.superhero.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.booleanThat;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import lv.naumov.lesson36.superhero.Superhero;
+import lv.naumov.lesson36.superhero.SuperheroManager;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(SuperheroManager.class) 
+public class SuperheroManagerTest {
+
+	private SuperheroManager superheroManager;
+
+	@BeforeEach
+	public void setUp() {
+		superheroManager = new SuperheroManager();
+	}
+
+	@Test
+	public void testAddSuperhero() {
+		Superhero superhero = new Superhero("Superman", 9, 35);
+		assertTrue(superheroManager.addSuperhero(superhero));
+	}
+
+	@Test
+	public void testAddSuperheroWithDuplicateName() {
+		Superhero superhero1 = new Superhero("Batman", 7, 40);
+		Superhero superhero2 = new Superhero("Batman", 8, 38);
+		assertTrue(superheroManager.addSuperhero(superhero1));
+		assertFalse(superheroManager.addSuperhero(superhero2));
+	}
+
+	@Test
+	public void testRemoveSuperhero() {
+		Superhero superhero = new Superhero("Wonder Woman", 8, 30);
+		superheroManager.addSuperhero(superhero);
+		assertTrue(superheroManager.removeSuperhero("Wonder Woman"));
+	}
+
+	@Test
+	public void testRemoveNonexistentSuperhero() {
+		assertFalse(superheroManager.removeSuperhero("Flash"));
+	}
+
+	@Test
+	public void testGetSuperhero() {
+		Superhero superhero = new Superhero("Aquaman", 6, 45);
+		superheroManager.addSuperhero(superhero);
+		Superhero retrievedSuperhero = superheroManager.getSuperhero("Aquaman");
+		assertNotNull(retrievedSuperhero);
+		assertEquals(superhero, retrievedSuperhero);
+	}
+
+	@Test
+	public void testGetNonexistentSuperhero() {
+		assertNull(superheroManager.getSuperhero("Green Lantern"));
+	}
+
+	@Test
+	public void testGetAllSuperheroes() {
+		Superhero superhero1 = new Superhero("Flash", 5, 28);
+		Superhero superhero2 = new Superhero("Green Lantern", 7, 33);
+		superheroManager.addSuperhero(superhero1);
+		superheroManager.addSuperhero(superhero2);
+		List<Superhero> allSuperheroes = superheroManager.getAllSuperheroes();
+		assertEquals(2, allSuperheroes.size());
+	}
+
+	@Test
+	public void testUpdateSuperhero() {
+		Superhero oldSuperhero = new Superhero("Hulk", 9, 40);
+		Superhero newSuperhero = new Superhero("Thor", 8, 35);
+		superheroManager.addSuperhero(oldSuperhero);
+		superheroManager.updateSuperhero("Hulk", newSuperhero);
+		assertEquals(newSuperhero, superheroManager.getSuperhero("Thor"));
+	}
+
+
+
+	@Test
+	public void testPrivateContainsSuperhero() throws Exception {
+	    // инстанцируем SuperheroManager
+	    SuperheroManager manager = new SuperheroManager();
+	    Superhero superhero = new Superhero("Tomb Rider", 9, 25);
+	    manager.addSuperhero(superhero);
+
+	    // получаем приватный метод
+	    Method containsSuperheroPrivateMethod = SuperheroManager.class.getDeclaredMethod("containsSuperhero", String.class);
+
+	    // меняем доступность метода
+	    containsSuperheroPrivateMethod.setAccessible(true);
+
+	    // теперь можем вызывать приватный метод
+	    boolean result = (boolean) containsSuperheroPrivateMethod.invoke(manager, "Superman");
+
+	    // У нас нет Томб Райдера, у нас Супермэн
+	    assertFalse(result);
+	    
+	    // добавляем супермэна
+	    Superhero superhero2 = new Superhero("Superman", 9, 40);
+	    manager.addSuperhero(superhero2);
+	    boolean result2 = (boolean) containsSuperheroPrivateMethod.invoke(manager, "Superman");
+	    assertTrue(result2);
+	    
+	    
+	    
+	}
+}
