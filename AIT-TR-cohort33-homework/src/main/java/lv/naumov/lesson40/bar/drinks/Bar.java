eff8cdcc546601bdfdf743da7be3c43957fe5f8c
Андрей Наумов <andrejs@list.ru>
2023-10-26 18:10:31 +0200

    lesson 40 -  rebased

--- AIT-TR-cohort33-homework/src/main/java/lv/naumov/lesson40/bar/drinks/Bar.java
+++ AIT-TR-cohort33-homework/src/main/java/lv/naumov/lesson40/bar/drinks/Bar.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package lv.naumov.lesson40.bar.drinks;
 
 import lombok.AllArgsConstructor;
@@ -33,29 +32,4 @@ public class Bar {
     }
 
 }
-=======
-package lv.naumov.lesson40.bar.drinks;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import lombok.extern.slf4j.XSlf4j;
-
-import java.util.HashMap;
-
-@Slf4j
-@AllArgsConstructor
-public class Bar {
-    private HashMap<String, Drink> drinkMap;
-
-    public void addDrink(String key, Drink drink) {
-        log.info("В Бар будет записано>" + key + " с названием> " + drink);
-        drinkMap.put(key,drink);
-    }
-    public Drink getDrink(String id) {
-        Drink drink = drinkMap.get(id);
-        log.info("Достаём из бара: " + drink);
-        return drink;
-    }
-
-}
->>>>>>> branch 'master' of https://github.com/andrej-naumov/AIT-TR-cohort33-homework.git
