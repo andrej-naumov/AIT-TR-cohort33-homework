3aede6fa70eb9a2ccab8887e9eabcfb79ba62491
Андрей Наумов <andrejs@list.ru>
2023-10-26 10:14:46 +0200

    add logging

--- AIT-TR-cohort33-homework/src/main/java/lv/naumov/lesson40/bar/drinks/Bar.java
+++ AIT-TR-cohort33-homework/src/main/java/lv/naumov/lesson40/bar/drinks/Bar.java
@@ -1,27 +1,24 @@
 package lv.naumov.lesson40.bar.drinks;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 
 import java.util.HashMap;
 
-/**
- * Внутри этого класса создайте HashMap,
- * где ключом будет являться String (ID напитка),
- * а значением — объект класса Drink.
- * <p>
- * Методы:
- * addDrink(String id, Drink drink): добавляет напиток в коллекцию.
- * getDrink(String id): возвращает напиток по ID.
- */
+@Slf4j
 @AllArgsConstructor
 public class Bar {
     private HashMap<String, Drink> drinkMap;
 
     public void addDrink(String key, Drink drink) {
+        log.info("В Бар будет записано>" + key + " с названием> " + drink);
         drinkMap.put(key,drink);
     }
     public Drink getDrink(String id) {
-        return drinkMap.get(id);
+        Drink drink = drinkMap.get(id);
+        log.info("Достаём из бара: " + drink);
+        return drink;
     }
 
 }
