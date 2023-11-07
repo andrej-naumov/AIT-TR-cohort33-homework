5363e373cd0eda9b7ed847696f610a8f744a958e
Andrej Naumov <andrejs@list.ru>
2023-11-07 10:50:23 +0100

    added lombok annotation

--- AIT-TR-cohort33-homework/src/main/java/lv/naumov/lombook/User.java
+++ AIT-TR-cohort33-homework/src/main/java/lv/naumov/lombook/User.java
@@ -1,16 +1,15 @@
 package lv.naumov.lombook;
 
-import java.time.LocalDate;
+import lombok.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.time.LocalDate;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class User {
 
 	private Long id;
