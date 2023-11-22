c7022d89bfdb334ced835495d1e3f71b33a6fdb2
Andrej Naumov <andrejs@list.ru>
2023-11-22 23:17:45 +0100

    added lesson 47

---
+++ AIT-TR-cohort33-homework/src/main/java/lv/naumov/lesson50/cmd/CreateDirectory.java
@@ -0,0 +1,25 @@
+package lv.naumov.lesson50.cmd;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CreateDirectory {
+    public static String mkdir(String directoryName)  {
+        File directory = new File(directoryName);
+        if(directory.exists()) {
+            return String.format("Директория '%s' уже существует.", directoryName);
+        }
+
+        try {
+            Path newDirectoryPath = Paths.get(directoryName);
+            Files.createDirectory(newDirectoryPath);
+            return String.format("Директория '%s' создана успешно.", directoryName);
+        } catch (IOException e) {
+            return ("Ошибка при создании директории: " + e.getMessage());
+        }
+    }
+
+}
