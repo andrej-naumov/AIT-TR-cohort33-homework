9eb9ba62f7406d22315aa461367bd474216e7eb1
Andrej Naumov <andrejs@list.ru>
2023-11-30 19:53:05 +0100

    Lesson 54 - added

---
+++ AIT-TR-cohort33-homework/src/main/java/lv/naumov/lesson54/enumeration/Album.java
@@ -0,0 +1,14 @@
+package lv.naumov.lesson54.enumeration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+class Album {
+    private final String title;
+    private final String artist;
+    private final MusicGenre genre;
+}
