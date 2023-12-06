bee95c0f87eeba93dc46173a9b3f7b7b78ee9859
Andrej Naumov <andrejs@list.ru>
2023-12-06 19:15:57 +0100

    added lesson 55 - одевайтесь красиво

---
+++ AIT-TR-cohort33-homework/src/main/java/lv/naumov/lesson55/fashion/Main.java
@@ -0,0 +1,41 @@
+package lv.naumov.lesson55.fashion;
+
+import java.util.EnumSet;
+
+public class Main {
+    public static void main(String[] args) {
+        FashionCatalog каталогМоды = new FashionCatalog();
+
+        // Пример добавления товаров в каталог
+        FashionItem товар1 = new FashionItem("Платье", 59.99, FashionCategory.ОДЕЖДА, EnumSet.of(Season.ЛЕТО, Season.ВЕСНА));
+        FashionItem товар2 = new FashionItem("Кроссовки", 79.99, FashionCategory.ОБУВЬ, EnumSet.of(Season.ЛЕТО, Season.ВЕСНА));
+        FashionItem товар3 = new FashionItem("Солнцезащитные очки", 29.99, FashionCategory.АКСЕССУАРЫ, EnumSet.of(Season.ЛЕТО));
+
+        каталогМоды.добавитьМодныйТовар(товар1);
+        каталогМоды.добавитьМодныйТовар(товар2);
+        каталогМоды.добавитьМодныйТовар(товар3);
+
+        // Пример вывода товаров для определенного сезона
+        // Mожно выполнить операции с каждым товаром, соответствующим сезону "ЛЕТО"
+        Season season = Season.ЛЕТО;
+        System.out.println(" --- Tовары для сезона " + season.name());
+        for(FashionItem fashionItem : каталогМоды.получитьТоварыПоСезону(season)){
+            System.out.println("Категория: " + fashionItem.getКатегория() +
+                    ", Название: " + fashionItem.getНазвание() +
+                    ", Цена: " + fashionItem.getЦена());
+        }
+
+        System.out.println(" ----- ");
+
+        // Пример вывода товаров для определенной категории
+        // Mожно выполнить операции с категорией одежда
+        FashionCategory fashionCategory = FashionCategory.ОДЕЖДА;
+        System.out.println(" --- Товары категории " + fashionCategory);
+        for (FashionItem fashionItem : каталогМоды.получитьТоварыПоКатегории(FashionCategory.ОДЕЖДА)     ) {
+            System.out.println("Категория: " + fashionItem.getКатегория() +
+                    ", Название: " + fashionItem.getНазвание() +
+                    ", Цена: " + fashionItem.getЦена());
+        }
+
+    }
+}
---
+++ AIT-TR-cohort33-homework/src/main/java/lv/naumov/lesson55/fashion/Season.java
@@ -0,0 +1,6 @@
+package lv.naumov.lesson55.fashion;
+
+// Перечисление времен года
+public enum Season {
+    ВЕСНА, ЛЕТО, ОСЕНЬ, ЗИМА
+}
