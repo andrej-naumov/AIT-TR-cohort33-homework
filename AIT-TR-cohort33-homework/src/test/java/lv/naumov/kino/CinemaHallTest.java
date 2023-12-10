fee0331257afc2f67f7e29dcc6d055e405944452
andrej-naumov <andrejs@list.ru>
2023-12-10 11:05:36 +0100

    added book test

--- AIT-TR-cohort33-homework/src/test/java/lv/naumov/kino/CinemaHallTest.java
+++ AIT-TR-cohort33-homework/src/test/java/lv/naumov/kino/CinemaHallTest.java
@@ -10,12 +10,31 @@ import java.time.LocalDateTime;
 public class CinemaHallTest {
     @Test
     public void testAddMovieToSchedule() {
-        CinemaHall cinemaHall = new CinemaHall(1, 100);
-        Movie movie = new Movie("Movie Title", LocalDateTime.now(), LocalDateTime.now().plusHours(2), 75);
+        String movieTitle = "Movie Title";
+        int seatsInHall = 100;
+        int soldTicket = 75;
+        CinemaHall cinemaHall = new CinemaHall(1, seatsInHall);
+        Movie movie = new Movie(movieTitle, LocalDateTime.now(), LocalDateTime.now().plusHours(2), soldTicket);
 
         cinemaHall.addMovie(movie);
         int availableSeats = cinemaHall.getAvailableSeats(movie);
 
-        Assertions.assertEquals(25, availableSeats);
+        Assertions.assertEquals(seatsInHall - soldTicket, availableSeats);
+        Assertions.assertEquals(movie.getTitle(), movieTitle);
     }
+
+    @Test
+    public void testBookTicket() throws SoldOutException {
+        String movieTitle = "Movie Title";
+        int seatsInHall = 100;
+        int soldTickets = 10;
+        int bookTickets = 10;
+        CinemaHall cinemaHall = new CinemaHall(2, seatsInHall);
+        Movie movie = new Movie(movieTitle, LocalDateTime.now(), LocalDateTime.now().plusHours(1), soldTickets);
+        cinemaHall.addMovie(movie);
+        cinemaHall.bookTicket(movie, bookTickets);
+        Assertions.assertEquals(movie.getSoldTickets(), soldTickets + bookTickets);
+        Assertions.assertEquals(cinemaHall.getAvailableSeats(movie), seatsInHall - soldTickets - bookTickets );
+    }
+
 }
